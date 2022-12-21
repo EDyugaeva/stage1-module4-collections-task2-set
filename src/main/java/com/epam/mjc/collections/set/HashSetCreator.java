472fe5533670c06be79de92ee22d9c14c686855c
EDyugaeva <ed2408@yandex.ru>
2022-12-21 21:25:46 +0500

    commit

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,21 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> result = new HashSet<>();
+        for (int i = 0; i < sourceList.size(); i++) {
+            int temp = sourceList.get(i);
+            if (temp % 2 == 0) {
+                while (temp % 2 == 0) {
+                    result.add(temp);
+                    temp = temp / 2;
+                }
+            }
+            if (temp % 2 != 0) {
+                result.add(temp);
+                result.add(temp * 2);
+            }
+        }
+
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.set;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        Set<Integer> setResult = new TreeSet<>();
+        for (Integer i :
+                sourceList) {
+            setResult.add(i * i);
+        }
+
+        SortedSet<Integer> newRes = ((TreeSet<Integer>) setResult).subSet(lowerBound, true,upperBound, true);
+
+
+        return newRes;
     }
+
 }
